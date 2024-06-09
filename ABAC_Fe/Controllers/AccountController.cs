02d4bf210104703b18240d8cf1508ba517cb66a1
still <qanh7661@gmail.com>
2024-06-09 22:05:08 +0700

    Revert "final"
    This reverts commit ccff6db7713c698fd82eba5a6fbb53d6ce977569.

--- ABAC_Fe/Controllers/AccountController.cs
+++ ABAC_Fe/Controllers/AccountController.cs
@@ -288,50 +288,6 @@ namespace ABAC_Fe.Controllers
                 return response.IsSuccessStatusCode;
             }
         }
-
-        public ActionResult IT()
-        {
-            return View();
-        }
-
-        public ActionResult HR()
-        {
-            return View();
-        }
-
-        public ActionResult Finance()
-        {
-            return View();
-        }
-        // Action to fetch resource content
-        public async Task<ActionResult> GetResourceContent(int resourceId)
-        {
-            if (Session["AuthToken"] == null)
-            {
-                return Json(new { success = false, message = "Unauthorized" }, JsonRequestBehavior.AllowGet);
-            }
-
-            var token = Session["AuthToken"] as string;
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-                var response = await client.GetAsync($"http://localhost:5291/api/access/resource/{resourceId}?action=read");
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = await response.Content.ReadAsStringAsync();
-                    return Json(new { success = true, content }, JsonRequestBehavior.AllowGet);
-                }
-                else
-                {
-                    return Json(new { success = false, message = "Failed to fetch content" }, JsonRequestBehavior.AllowGet);
-                }
-            }
-        }
-
-
-
         public class ProfileInfo
         {
             public int Id { get; set; }
